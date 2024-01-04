a9c8038e54ce8e8137cbd8f8a95d40b24ef39ca5
phuongtrieu97coder <phuongcoderinfinity@gmail.com>
2024-01-03 20:55:48 -0800

    cmit_01_03_2024_8:55:PM

---
+++ Codecademy/Interstellar_Travel/src/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+// Create the function fuelGauge() here
+func fuelGauge(fuel int){
+  fmt.Println("You have",fuel," fuel left")
+}
+
+// Create the function calculateFuel() here
+func calculateFuel(planet string) int{
+  var fuel int
+  switch planet {
+    case "Venus":
+      fuel = 300000
+    case "Mercury":
+      fuel = 500000
+    case "Mars":
+      fuel = 700000
+    default:
+      fuel = 0
+  }
+  return fuel
+}
+
+// Create the function greetPlanet() here
+func greetPlanet(planet string){
+  fmt.Println("Welcome to planet",planet)
+}
+
+// Create the function cantFly() here
+func cantFly(){
+  fmt.Println("We do not have the available fuel to fly there.")
+}
+
+// Create the function flyToPlanet() here
+func flyToPlanet(planet string, fuel int) int {
+  var fuelRemaining, fuelCost int
+  fuelRemaining = fuel
+  fuelCost = calculateFuel(planet)
+
+  if fuelRemaining >= fuelCost {
+    greetPlanet(planet)
+    fuelRemaining-=fuelCost
+  }else{
+    cantFly()
+  }
+  return fuelRemaining
+}
+
+func main() {
+  // Test your functions!
+  
+  // Create `planetChoice` and `fuel`
+  var fuel int = 1000000
+  planetChoice := "Venus"
+
+  fuel = flyToPlanet(planetChoice,fuel)
+  fuelGauge(fuel)
+  // And then liftoff!
+  
+}
